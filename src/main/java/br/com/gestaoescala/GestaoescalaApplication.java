4971345a405a4e552180f82b2eade19076bc89db
ednezer <godoirezende@gmail.com>
2019-08-17 19:36:39 -0300

    Primeiro commit - criando projeto

---
+++ src/main/java/br/com/gestaoescala/GestaoescalaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaoescala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoescalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoescalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaoescala/GestaoescalaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoescala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoescalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
